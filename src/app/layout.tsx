188c511a1627e16eb31c0327e1901c9800227ea6
Chirayu Agrawal <chirayu.a@westagilelabs.com>
2023-06-13 20:16:26 +0530

    Fix localStoage Mount

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,23 +5,34 @@ import "./globals.css";
 import { UserProvider } from "@/utils/contexts";
 import { Toaster } from "react-hot-toast";
 import ProtectedRoutes from "@/utils/ProtectedRoutes";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const [render, setRender] = useState(false);
 	const pathname = usePathname();
 	const notNavbarDisplayRoutes = ["/signup", "/login"];
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			// this makes sure the window is mounted before rendering since localstorage requires window for operation
+			setRender(true);
+		}
+	}, []);
 
 	return (
 		<html lang="en">
 			<body>
-				<UserProvider>
-					{notNavbarDisplayRoutes.includes(pathname) ? null : <Navbar />}
-					<Toaster position="top-right" reverseOrder={false} />
-					<ProtectedRoutes>{children}</ProtectedRoutes>
-				</UserProvider>
+				{render && (
+					<UserProvider>
+						{notNavbarDisplayRoutes.includes(pathname) ? null : <Navbar />}
+						<Toaster position="top-right" reverseOrder={false} />
+						<ProtectedRoutes>{children}</ProtectedRoutes>
+					</UserProvider>
+				)}
 			</body>
 		</html>
 	);
